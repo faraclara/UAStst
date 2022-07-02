7d3a981ee8cdb0867b6fa21ff21acb37423b474c
faraclara <faraadevianaaa@gmail.com>
2022-07-02 18:27:12 +0700

    first commit

---
+++ ci4/app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $model = model('UserModel');
+		$model->insert([
+		'username' => 'farafara',
+		'useremail' => 'farafara@gmail.com',
+		'userpassword' => password_hash('catscats', PASSWORD_DEFAULT),
+		]);
+    }
+}
---
+++ ci4/app/Views/template/admin_footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <p>&copy; Universitas Pelita Bangsa 2021 - Fara Deviana - 312010407 - TI.20.A.2</p>
+    </footer>
+    </div>
+</body>
+</html>
